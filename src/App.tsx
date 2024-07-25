9365cd8893dca748bc3742261d7a0866728dc92d
Jessica <jessica_costaignacio@hotmail.com>
2024-07-25 20:08:16 -0300

    settings

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,9 @@
-// import Sidebar from './components/Sidebar';
 import LoadingPage from './pages/loadingPage/LoadingPage';
 import './styles.scss';
 
 function App() {
   return (
     <>
-      {/* <Sidebar /> */}
-
-      {/* scss working test example */}
-      {/* <div className="app">
-        <h1>Hello, world!</h1>
-      </div> */}
       <LoadingPage/>
     </>
     
